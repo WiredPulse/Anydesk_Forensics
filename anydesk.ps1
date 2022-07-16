3c0e89606bbcf9f90cbe3dbf070c5815b14749fc
Fernando Tomlinson <WiredPulse@users.noreply.github.com>
2022-07-16 18:02:59 -0400

    Add files via upload

--- anydesk.ps1
+++ anydesk.ps1
@@ -1,4 +1,22 @@
-﻿function connections ($logfile){
+﻿function Get-ADConnections{
+<#
+.SYNOPSIS
+    Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns a list of incoming 
+    and outgoing connections, to include the timstamps, AnyDesk IDs, IP addresses, and PIDs.
+
+.PARAMETER logFile
+    Location to the ad_svc.trace or ad.trace log.
+
+.EXAMPLE
+    Get-ADkConnections -logFile c:\ad_svc.trace
+
+    Parses C:\ad_svc.trace and returns a list of incoming and outgoing connection data.
+#>
+    [CmdletBinding()]
+    param(
+        $logfile
+    )
+
     $log = Get-Content $logfile
     $obj = @()
     $obj = foreach($line in $log){
@@ -43,8 +61,26 @@
     $obj
 }
 
+function Get-ADOutgoingConnections{
+<#
+    .SYNOPSIS
+        Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns a list of outgoing connections, to 
+        include the timestamps, AnyDesk IDs, and PIDs.
 
-function outgoingConnections($logfile){
+    .PARAMETER logFile
+        Location to the ad_svc.trace or ad.trace log.
+
+    .EXAMPLE
+        Get-ADOutgoingConnections -logFile c:\ad_svc.trace
+
+        Parses C:\ad_svc.trace and returns a list of outgoing connection data.
+#>
+    
+    [CmdletBinding()]
+    param(
+        $logfile
+    )
+
     $log = Get-Content $logfile
     $lines = $log | Select-String -Pattern 'app.session - 1: Connecting to "'
     $obj = @()
@@ -59,7 +95,26 @@ function outgoingConnections($logfile){
     $obj
 }
 
-function incomingConnectionsIDs ($logfile){
+function Get-ADIncomingConnections{
+<#
+    .SYNOPSIS
+        Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns a list of incoming connections, to 
+        include the timestamps, AnyDesk IDs, and PIDs.
+
+    .PARAMETER logFile
+        Location to the ad_svc.trace or ad.trace log.
+
+    .EXAMPLE
+        Get-ADIncomingConnections -logFile c:\ad_svc.trace
+
+        Parses C:\ad_svc.trace and returns a list of incoming connection data.
+#>
+    
+    [CmdletBinding()]
+    param(
+        $logfile
+    )
+
     $log = Get-Content $logfile
     $lines = $log | Select-String -Pattern 'accept request from'
     $obj = @()
@@ -74,8 +129,26 @@ function incomingConnectionsIDs ($logfile){
     $obj
 }
 
+function Get-ADIncomingConnectionIPs{
+<#
+    .SYNOPSIS
+        Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns a list of IP addresses associated 
+        with incoming connections.
 
-function incomingConnectionsIPs ($logfile){
+    .PARAMETER logFile
+        Location to the ad_svc.trace or ad.trace log.
+
+    .EXAMPLE
+        Get-ADIncomingConnectionIPs -logFile c:\ad_svc.trace
+
+        Parses C:\ad_svc.trace and returns a list of timestamps and IP addresses.
+#>
+    
+    [CmdletBinding()]
+    param(
+        $logfile
+    )
+
     $log = Get-Content $logfile
     $lines = $log | Select-String -Pattern 'accept request from'
     $obj = @()
@@ -95,7 +168,26 @@ function incomingConnectionsIPs ($logfile){
     $obj
 }
 
-function UniqueincomingConnectionsIPs ($logfile){
+function Get-ADUniqueIncomingConnectionIPs{
+<#
+    .SYNOPSIS
+        Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns a list of unique IP addresses 
+        associated with incoming connections.
+
+    .PARAMETER logFile
+        Location to the ad_svc.trace or ad.trace log.
+
+    .EXAMPLE
+        Get-ADUniqueIncomingConnectionIPs -logFile c:\ad_svc.trace
+
+        Parses C:\ad_svc.trace and returns a list of unique IP addresses.
+#>
+    
+    [CmdletBinding()]
+    param(
+        $logfile
+    )
+
     $log = Get-Content $logfile
     $lines = $log | Select-String -Pattern 'accept request from'
     $obj = @()
@@ -115,7 +207,27 @@ function UniqueincomingConnectionsIPs ($logfile){
     $obj.incomingIP | Sort-Object -Unique
 }
 
-function duration ($logfile){
+function Get-ADConnectionDurations{
+<#
+    .SYNOPSIS
+        Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns a list of incoming connections and 
+        their durations to include timestamps.
+
+
+    .PARAMETER logFile
+        Location to the ad_svc.trace or ad.trace log.
+
+    .EXAMPLE
+        Get-ADConnectionDurations -logFile c:\ad_svc.trace
+
+        Parses C:\ad_svc.trace and returns each connection and its duration
+#>
+    
+    [CmdletBinding()]
+    param(
+        $logfile
+    )
+
     $log = Get-Content $logfile
     $end = $log.IndexOf($log[-1])
     $lines = $log | Select-String -Pattern 'accept request from'
@@ -152,19 +264,36 @@ function duration ($logfile){
     $obj
 }
 
+function Get-ADTunnel{
+<#
+    .SYNOPSIS
+        Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns a list of data about any 
+        configured AnyDesk tunnel configurations
 
-function tunnel($logfile){
+    .PARAMETER logFile
+        Location to the ad_svc.trace or ad.trace log.
+
+    .EXAMPLE
+        Get-ADTunnel -logFile c:\ad_svc.trace
+
+        Parses C:\ad_svc.trace and returns tunnel information
+#>
+    
+    [CmdletBinding()]
+    param(
+        $logfile
+    )
     $log = Get-Content $logfile
     $obj = @()
     $obj = foreach($line in $log){
-        $split = $line -split ' '
-        $remote = $split[-1] -split ':'
         if($line -like "*tunnel.config*->*"){
+            $split = $line -split ' '
+            $remote = $split[-1] -split ':'
             [pscustomobject]@{
-            Timestamp = ([datetime]($split[4]+' '+$split[5])).ToString("yyyy-MM-dd HH:mm:ss")
-            LocalPort = $split[-3]
-            RemoteIP = $remote[0]
-            RemotePort = $remote[-1]
+                Timestamp = ([datetime]($split[4]+' '+$split[5])).ToString("yyyy-MM-dd HH:mm:ss")
+                LocalPort = $split[-3]
+                RemoteIP = $remote[0]
+                RemotePort = $remote[-1]
             }
         }
     }
@@ -173,15 +302,65 @@ function tunnel($logfile){
 
 
 
-           # pause
-            for($i = $index; $i -lt $end; $i++){
-               write-host $i
-                if($log[$i] -like "*Cleaned up 1/1 managed client connections*"){
-                    1
-                    $line
-                    $log[$i]
-                    
-                }
-            }
-        #}
+function Get-ADTunnelConnections{
+<#
+    .SYNOPSIS
+        Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns a list of tunnels that were
+        configured and used to include the timestamps associated with the connections. 
+
+
+    .PARAMETER logFile
+        Location to the ad_svc.trace or ad.trace log.
+
+    .EXAMPLE
+        Get-Get-ADTunnelConnections -logFile c:\ad_svc.trace
+
+        Parses C:\ad_svc.trace and returns tunnel data
+#>
+    
+    [CmdletBinding()]
+    param(
+        $logfile
+    )
+
+    $log = Get-Content $logfile
+    $end = $log.IndexOf($log[-1])
+    $lines = $log | Select-String -Pattern 'app.session - 1: Connecting to "'
+    $obj = $tun = @()
+    $obj = foreach($line in $lines){
+        $tunnelTimestamp = ''
+        $startSplit = $line -split ' '
+        $connectTimestamp = ([datetime]($startSplit[4]+' '+$startSplit[5])).ToString("yyyy-MM-dd HH:mm:ss")
+        for($i = ($line.Linenumber); $i -lt $end; $i++){
+            if($log[$i] -like "*tunneling_config*->*"){ 
+                $split = $log[$i] -split ' '
+                $remote = $split[-1] -split ':'
+                break
+            }           
+        }
+        if($tunnelTimestamp.Length -eq 0){
+            $tunnelTimestamp = 'n'
+        }
+        [pscustomobject]@{
+            Timestamp = $connectTimestamp
+            OutgoingID = ($startSplit[-1]).Trim('"')
+            LocalPort = $split[-3]
+            RemoteIP = $remote[0]
+            RemotePort = $remote[-1]
+            TunnelTimestamp =$tunnelTimestamp
+        }   
     }
+
+    $tunConnect = $log | select-string -Pattern "accepting tcp-tunnel"
+    foreach($line in $tunConnect){
+        $split = $line -split ' '
+        $date = ([datetime]($split[4]+' '+$split[5])).ToString("yyyy-MM-dd HH:mm:ss")
+        $index = 0
+        foreach($item in $obj){
+            if($date -gt $item.timestamp -AND $date -lt $obj[($index + 1)].timestamp){
+                $item.TunnelTimestamp = $date
+            }
+            $index += 1
+        }
+    }
+}
