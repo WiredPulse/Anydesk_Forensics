fb579a8d371699e29e11acb2496194a2fb0a3c07
Fernando Tomlinson <WiredPulse@users.noreply.github.com>
2022-07-19 08:12:25 -0400

    Add files via upload

--- anydesk.ps1
+++ anydesk.ps1
@@ -429,7 +429,7 @@ function Get-ADFileTransferTo{
 <#
     .SYNOPSIS
         Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns a list directories from which a 
-        file or files were transferrred from to a remote system. The list also includes the timstamp of the AnyDesk connection, the remote anydesk ID and IP, as well as
+        file or files were transferrred from to a remote system. The list also includes the timestamp of the AnyDesk connection, the remote anydesk ID and IP, as well as
         the transfer timestamp.
 
     .PARAMETER logFile
@@ -438,13 +438,14 @@ function Get-ADFileTransferTo{
     .EXAMPLE
         Get-ADFileTransferTo -logFile c:\ad_svc.trace
 
-        Parses C:\ad_svc.trace and returns tunnel data
+        Parses C:\ad_svc.trace and returns the applicable data
 #>
     
     [CmdletBinding()]
     param(
         $logfile
-    )    
+    )  
+      
     $log = Get-Content $logfile
     $lines = $log | Select-String -Pattern 'accept request from'
     $obj = @()
@@ -483,10 +484,197 @@ function Get-ADFileTransferTo{
 }
 
 function Get-ADFileTransferFrom{
+<#
+    .SYNOPSIS
+        Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns the timestamp of when a file
+        was transferred from the local system to the remote system with the AnyDesk connection. The data returned also includes the connection timestamp, connected ID, and Connected IP.
 
+    .PARAMETER logFile
+        Location to the ad_svc.trace or ad.trace log.
 
+    .EXAMPLE
+        Get-ADFileTransferFrom -logFile c:\ad_svc.trace
+
+        Parses C:\ad_svc.trace and returns the applicable data
+#>
+    
+    [CmdletBinding()]
+    param(
+        $logfile
+    )    
+
+    $log = Get-Content $logfile
+    $lines = $log | Select-String -Pattern 'accept request from'
+    $obj = @()
+    $obj = foreach($line in $lines){
+        $split = $line -split ' '
+        $index = $log.IndexOf($line) + 20
+        for($i = $index; $i -gt ($index - 20); $i--){
+            if($log[$i] -match "anynet.any_socket - Logged in from "){
+                $netData = (($log[$i] -split ' ')[-4]) -split ':'
+                break
+            }           
+        }
+        [pscustomobject]@{
+            Timestamp = ([datetime]($split[4]+' '+$split[5])).ToString("yyyy-MM-dd HH:mm:ss")
+            ConnectedID = $split[-3]
+            ConnectedIP = $netData[0]
+            TransferTimestamp = '-'
+        }
+    }
+
+    $fileTask = $log | select-string -Pattern "file_transfer - Download finished"
+    foreach($line in $fileTask){
+        $split = $line -split ' '
+        $date = ([datetime]($split[4]+' '+$split[5])).ToString("yyyy-MM-dd HH:mm:ss")
+        $index = 0
+        foreach($item in $obj){
+            if($date -gt $item.timestamp -AND $date -lt $obj[($index + 1)].timestamp){
+                $item.TransferTimestamp = $date
+            }
+            $index += 1
+        }
+    }
+    $obj | Format-Table
 }
 
-function keyboard{
-    desk_rt.capture_component - Keyboard layout changed to: en_us
+function Get-ADKeyboardLayout{
+<#
+    .SYNOPSIS
+        Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns the keyboard layout associated 
+        with the connection. The data returned also includes the connection timestamp, connected ID, and Connected IP.
+
+    .PARAMETER logFile
+        Location to the ad_svc.trace or ad.trace log.
+
+    .EXAMPLE
+        Get-ADKeyboardLayout -logFile c:\ad_svc.trace
+
+        Parses C:\ad_svc.trace and returns the applicable data
+#>
+    
+    [CmdletBinding()]
+    param(
+        $logfile
+    )        
+    
+    $log = Get-Content $logfile
+    $lines = $log | select-string -pattern 'accept request from'
+    $obj = @()
+    $obj = foreach($line in $lines){
+        $split = $line -split ' '
+        $index = $log.IndexOf($line) + 20
+        for($i = $index; $i -gt ($index - 20); $i--){
+            if($log[$i] -match "anynet.any_socket - Logged in from "){
+                $netData = (($log[$i] -split ' ')[-4]) -split ':'
+                break
+            }           
+        }
+        [pscustomobject]@{
+            Timestamp = ([datetime]($split[4]+' '+$split[5])).ToString("yyyy-MM-dd HH:mm:ss")
+            ConnectedID = $split[-3]
+            ConnectedIP = $netData[0]
+            KeyboardLayout = '-'
+        }
+    }
+
+
+    $keyboard = $log | select-string -pattern "desk_rt.capture_component - Keyboard layout changed to: en_us"
+    foreach($line in $keyboard){
+        $split = $line -split ' '
+        $date = ([datetime]($split[4]+' '+$split[5])).ToString("yyyy-MM-dd HH:mm:ss")
+        $index = 0
+        foreach($item in $obj){
+            if($date -gt $item.timestamp -AND $date -lt $obj[($index + 1)].timestamp){
+                $item.keyboardlayout = $split[-1]
+            }
+            $index += 1
+        }
+    }
+    $obj | Format-Table
+}
+
+function Get-ADFileCopy{
+<#
+    .SYNOPSIS
+        Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns the number of files copied from the system and pasted to another system. The list also
+        includes the timestamp of the AnyDesk connection, the remote IP that the files were copied to, as well as the transfer timestamp.
+
+    .PARAMETER logFile
+        Location to the ad_svc.trace or ad.trace log.
+
+    .EXAMPLE
+        Get-ADFileTransferTo -logFile c:\ad_svc.trace
+
+        Parses C:\ad_svc.trace and returns the applicable data
+#>
+    
+    [CmdletBinding()]
+    param(
+        $logfile
+    )    
+
+    $log = Get-Content $logfile
+    $lines = $log | Select-String -Pattern 'clipbrd.capture - Found '
+    $obj = @()
+    $obj = foreach($line in $lines){
+        $split = $line -split ' '
+        $index = $log.IndexOf($line) + 1
+        $fileTime = $log[$index].Split(' ')
+        if($log[$index] -match 'clipbrd.capture - Relaying file offers'){
+            for($i = $log.IndexOf($log[$index]); $i -gt 0; $i--){
+                Remove-Variable connTime -ErrorAction SilentlyContinue
+                if($log[$i] -match 'anynet.any_socket - Logged in from'){
+                    $connTime = $log[$i] -split ' '
+                   # break
+                
+                    for($i2 = $i; $i2 -gt 0; $i2--){
+                        if($log[$i2] -match 'Accept request from'){
+                            [pscustomobject]@{
+                                ConnectionTimestamp = ([datetime]($connTime[4]+' '+$connTime[5])).ToString("yyyy-MM-dd HH:mm:ss")
+                                TransferredToIP = (($connTime[-4]) -split ':')[0]
+                                ID = ($log[$i2] -split ' ')[-3]
+                                TransferredTimestamp = ([datetime]($fileTime[4]+' '+$filetime[5])).ToString("yyyy-MM-dd HH:mm:ss")
+                                Files = $split[-2]
+                            }
+                            break
+                        }
+                    }
+                break
+                }
+            }
+        }
+    }
+    $obj | Format-Table
+}
+
+
+function Get-ADFilePaste{
+<#
+    .SYNOPSIS
+        Parses the ad_svc.trace log (for installed instances of AnyDesk) or ad.trace log (for portable instances of AnyDesk) and returns the the timestamp associated with a file being "pasted" from being 
+        copied from a remote AnyDesk session.
+
+    .PARAMETER logFile
+        Location to the ad_svc.trace or ad.trace log.
+
+    .EXAMPLE
+        Get-ADFileTransferTo -logFile c:\ad_svc.trace
+
+        Parses C:\ad_svc.trace and returns the applicable data
+#>
+
+    $log = Get-Content $logfile
+    $lines = $log | Select-String -Pattern 'ole.files - Finished file paste operation'
+    $obj = @()
+    $obj = foreach($line in $lines){
+        $split = $line -split ' '
+        $index = $log.IndexOf($line) + 1
+        $fileTime = $log[$index].Split(' ')
+
+        [pscustomobject]@{
+            TransferredTimestamp = ([datetime]($fileTime[4]+' '+$filetime[5])).ToString("yyyy-MM-dd HH:mm:ss")
+        }
+    }
+    $obj | Format-Table
 }
